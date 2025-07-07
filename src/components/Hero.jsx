5eb8fd173c7f5f3fc23d00770794a0e800514a8a
ahmedanwar1234 <ahmedanswar123@gmail.com>
2025-07-07 19:28:38 +0300

    test3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const shadowRef=useRef()
     scrollTrigger: {
       trigger: boomRef.current,
       start: 'bottom bottom',
-      end: isMobile ? '+=150' : '+=500', // ✅ الفرق هنا    },
+      end: isMobile ? '+=300' : '+=500', // ✅ الفرق هنا    },
   }})
 
   tl.to(boomRef.current, {
